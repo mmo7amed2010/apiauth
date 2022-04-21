97b49cf7b04e8f57df2549d4c6a992bb4f663339
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-22 01:06:16 +0200

    base code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '19352e0fbdb1f4f319595eada1496d554cbd8d9b',
+        'name' => 'mmo7amed2010/apiauth',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mmo7amed2010/apiauth' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '19352e0fbdb1f4f319595eada1496d554cbd8d9b',
+            'dev_requirement' => false,
+        ),
+    ),
+);
