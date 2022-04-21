19352e0fbdb1f4f319595eada1496d554cbd8d9b
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-22 00:59:32 +0200

    initial commit

---
+++ src/Apiauth.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mmo7amed2010\Apiauth;
+
+class Apiauth
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
