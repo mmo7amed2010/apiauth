d88ab61f8acd32ac616aa1168fbe398ae56b9bb3
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-22 02:30:23 +0200

    trait

--- src/ApiauthTrait.php
+++ src/ApiauthTrait.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
-trait Apiauth
+trait ApiauthTrait
 {
 
     public static function routes()
