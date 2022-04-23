e38402c850f76a54c2362abc8ec76983e2c5c651
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-23 18:24:18 +0200

    add allowed routes config

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Mmo7amed2010\Apiauth\Apiauth;
 
-Route::group(['prefix' => config('apiauth.routes.routes_prefix') ?? "api", 'middleware' =>config('apiauth.routes.routes_middlewares') ?? ["api"]], function() {
-    Route::post('/login/token', Apiauth::loginByToken());
-    Route::middleware('auth:api')->post('/logout', Apiauth::logout());
-    Route::post('/refresh', Apiauth::refresh());
-    Route::post('/register', Apiauth::register());
-    Route::middleware('auth:api')->get('/user', Apiauth::user());
+if(config("apiauth.routes.allowed") == true){
+    Route::group(['prefix' => config('apiauth.routes.routes_prefix') ?? "api", 'middleware' =>config('apiauth.routes.routes_middlewares') ?? ["api"]], function() {
+        Route::post('/login/token', Apiauth::loginByToken());
+        Route::middleware('auth:api')->post('/logout', Apiauth::logout());
+        Route::post('/refresh', Apiauth::refresh());
+        Route::post('/register', Apiauth::register());
+        Route::middleware('auth:api')->get('/user', Apiauth::user());
 
-});
+    });
+}
