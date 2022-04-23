08ecc2cf77cbfb89d4617c584761275e57fa34dd
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-23 17:54:39 +0200

    move routes and config files

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Mmo7amed2010\Apiauth\Apiauth;
-Route::post('/login/token', Apiauth::loginByToken());
-Route::middleware('auth:api')->post('/logout', Apiauth::logout());
-Route::post('/refresh', Apiauth::refresh());
-Route::post('/register', Apiauth::register());
-Route::middleware('auth:api')->get('/user', Apiauth::user());
+
+Route::group(['prefix' => 'api', 'middleware' => 'api'], function() {
+
+    Route::post('/login/token', Apiauth::loginByToken());
+    Route::middleware('auth:api')->post('/logout', Apiauth::logout());
+    Route::post('/refresh', Apiauth::refresh());
+    Route::post('/register', Apiauth::register());
+    Route::middleware('auth:api')->get('/user', Apiauth::user());
+
+});
