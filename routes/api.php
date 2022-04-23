c2ff3d50f52b0916fd9e683bb1fb2cecf2036220
mohamed gamal <gemy@mohameds-MacBook-Air.local>
2022-04-23 17:58:18 +0200

    api prefix and middleware

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use Mmo7amed2010\Apiauth\Apiauth;
 
 Route::group(['prefix' => 'api', 'middleware' => 'api'], function() {
-
     Route::post('/login/token', Apiauth::loginByToken());
     Route::middleware('auth:api')->post('/logout', Apiauth::logout());
     Route::post('/refresh', Apiauth::refresh());
